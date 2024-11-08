9831a124c0d743dc7b41ff53d8f3e3b5ade0ff81
shahramteimoory <Darn.shan@yahoo.com>
2024-11-08 19:58:09 +0330

    Add project files.

---
+++ GrpcServer/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GrpcServer.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
